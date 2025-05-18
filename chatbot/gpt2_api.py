49c1fb82a47f8dd1f8bbdf8d82b3db7dae3a2ee0
KhanhDang21 <qxdangvietanh@gmail.com>
2025-05-18 23:58:30 +0700

    Update model chatbot

--- chatbot/gpt2_api.py
+++ chatbot/gpt2_api.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Request
 from pydantic import BaseModel
 from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import GPT2Tokenizer, GPT2LMHeadModel
 import torch
 
 app = FastAPI()
 
-tokenizer = AutoTokenizer.from_pretrained("gpt2")
-model = AutoModelForCausalLM.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained('NlpHUST/gpt2-vietnamese')
+model = GPT2LMHeadModel.from_pretrained('NlpHUST/gpt2-vietnamese')
 
 class Prompt(BaseModel):
     prompt: str
@@ -15,14 +16,15 @@ class Prompt(BaseModel):
 def generate_text(prompt: Prompt):
     input_ids = tokenizer.encode(prompt.prompt, return_tensors="pt")
     output = model.generate(
-        input_ids,
-        max_length=100,
+        input_ids,pad_token_id=tokenizer.eos_token_id,
         do_sample=True,
-        temperature=0.8,
-        top_k=50,
-        top_p=0.95,
-        no_repeat_ngram_size=3,
-        num_return_sequences=1
+        max_length=100,
+        min_length=100,
+        top_k=40,
+        num_beams=5,
+        early_stopping=True,
+        no_repeat_ngram_size=2,
+        num_return_sequences=3
     )
     result = tokenizer.decode(output[0], skip_special_tokens=True)
     return {"result": result}
